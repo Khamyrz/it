1c17cf43f50bca4f440e291d8db5afdbe451c01f
Khamyrz <Khamyrbautista@gmail.com>
2025-09-29 11:35:29 +0800

    dfd

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'Mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
